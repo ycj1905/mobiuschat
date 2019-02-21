d72457f666270451bf72d7e8c2077fc8afadbd43
Matt Jhou <ycj1665@gmail.com>
2019-02-21 11:48:40 +0800

    server bug fix

--- server/server.js
+++ server/server.js
@@ -74,7 +74,7 @@ io.on('connection', (socket) => {
   //     io.to(user.room).emit('updateUserList', users.getUserList(user.room));
   //     io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
   //   }
-  // });
+  });
 });
 
 server.listen(port, () => {
