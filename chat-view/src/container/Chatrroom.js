15cc18d60023e1b62bf12085a904b6ab6ebae886
Matt Jhou <ycj1665@gmail.com>
2019-02-21 17:37:07 +0800

    login without fb..

--- chat-view/src/container/Chatrroom.js
+++ chat-view/src/container/Chatrroom.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 
+const handleClick = () => {
+    axios.post('http://localhost:8080/api/createuser', {
+        facebookId: 'Matt'
+    }).then(
+        (s) => {console.log(s)},
+        (s) => {console.error(s)}
+    )
+}
 const Chatroom = () => {
     return (
         <div>
-            Chatroom
+            <button onClick={handleClick}>post</button>
         </div>
     )
 }
