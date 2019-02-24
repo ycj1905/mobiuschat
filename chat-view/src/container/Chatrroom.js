d1abe93819d6aa40b005f0c17e44e5c7f93cb43c
Matt Jhou <ycj1665@gmail.com>
2019-02-24 14:47:57 +0800

    jwt

--- chat-view/src/container/Chatrroom.js
+++ chat-view/src/container/Chatrroom.js
@@ -23,15 +23,45 @@ const handleLogin = () => {
         city: 'Taipei',
 
     }).then(
-        (s) => {console.log(s)},
+        (s) => {
+            // console.log(s.data.token)
+            localStorage.setItem('jwt_token', s.data.token);
+        },
         (s) => {console.error(s)}
     )
 }
+const handleProtected = () => {
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('jwt_token');
+    axios.get('http://localhost:8080/api/protected').then(
+        (s) => {
+            console.log('s')
+            console.log(s)
+        },
+        (s) => {
+            console.log('e')
+            console.log(s)
+        }
+    )
+}
+// const handleLogin = () => {
+//     axios.post('http://localhost:8080/api/user/login', {
+//         name: 'Matt',
+//         pswd: 'pswdpswd',
+//         gender: 'gender',
+//         email: 'ycj1665@gmail.com',
+//         city: 'Taipei',
+
+//     }).then(
+//         (s) => {console.log(s)},
+//         (s) => {console.error(s)}
+//     )
+// }
 const Chatroom = () => {
     return (
         <div>
             <button onClick={handleRegister}>register</button>
             <button onClick={handleLogin}>login</button>
+            <button onClick={handleProtected}>protected</button>
         </div>
     )
 }
